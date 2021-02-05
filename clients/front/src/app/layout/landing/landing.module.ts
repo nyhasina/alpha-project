05d20d40335c2316a79239b3f8177a2daaa75b33
Manjato Nick <manjato25@gmail.com>
2021-02-05 23:05:52 +0300

    add component header

--- clients/front/src/app/layout/landing/landing.module.ts
+++ clients/front/src/app/layout/landing/landing.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-    declarations: [LandingComponent],
+    declarations: [LandingComponent, HeaderComponent],
     imports: [CommonModule, LandingRoutingModule],
 })
 export class LandingModule {}

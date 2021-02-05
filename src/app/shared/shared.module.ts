b425c95252b7fdbcec59cfab5317fd6314ecf58c
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-05 17:40:23 +0300

    Register and export reactive form module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule],
+    imports: [CommonModule, ReactiveFormsModule],
+    exports: [ReactiveFormsModule],
 })
 export class SharedModule {}

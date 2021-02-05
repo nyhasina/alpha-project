d172bfa4739aad8a208e8b61b4d5216dd6190191
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-05 15:52:15 +0300

    Setup core store

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,19 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { CoreEffects } from './store/core.effects';
+import { reducers } from './store/core.reducer';
+import { CoreRouterEffects } from './store/core.router-effects';
 
 @NgModule({
     declarations: [],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        StoreModule.forRoot(reducers),
+        EffectsModule.forRoot([CoreEffects, CoreRouterEffects]),
+        StoreRouterConnectingModule.forRoot(),
+    ],
 })
 export class CoreModule {}

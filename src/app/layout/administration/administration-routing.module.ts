8be8491a06240b2e401a3338e585a9cf53310a08
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-05 11:51:08 +0300

    Create administration routing

---
+++ src/app/layout/administration/administration-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdministrationComponent } from './administration.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdministrationComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('../../pages/administration/dashboard/dashboard.module').then(m => m.DashboardModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdministrationRoutingModule {
+}

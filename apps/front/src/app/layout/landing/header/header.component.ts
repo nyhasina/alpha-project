3c55c1fe11d69b2e2d10596ac667bf24969f42fd
Manjato Nick <manjato25@gmail.com>
2021-02-06 20:35:59 +0300

    add font awesome

--- apps/front/src/app/layout/landing/header/header.component.ts
+++ apps/front/src/app/layout/landing/header/header.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  menu = ['Tournaments', 'Training', 'Game', 'Comminuty'];
+  navbarOpen = false;
   constructor() { }
-
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
   ngOnInit(): void {
   }
 

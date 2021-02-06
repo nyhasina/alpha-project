cb8bd5cc6b71097e08b5f23034e102efa41f2e5c
Manjato Nick <manjato25@gmail.com>
2021-02-06 21:55:49 +0300

    add component login

--- apps/front/src/app/layout/landing/landing-routing.module.ts
+++ apps/front/src/app/layout/landing/landing-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LandingComponent } from './landing.component';
+import {LoginComponent} from './login/login.component'
 
 const routes: Routes = [
     {
         path: '',
         component: LandingComponent,
     },
+    {
+        path: 'login',
+        component: LoginComponent,
+    }
 ];
 
 @NgModule({
--- apps/front/src/app/layout/landing/landing.module.ts
+++ apps/front/src/app/layout/landing/landing.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-    declarations: [LandingComponent, HeaderComponent],
+    declarations: [LandingComponent, HeaderComponent, LoginComponent],
     imports: [CommonModule, LandingRoutingModule],
 })
 export class LandingModule {}

fcd2a622d2d1a955588fa434bcfe927fbbb810a5
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-06 14:35:26 +0300

    Install nx cli

--- clients/front/src/app/pages/contact/contact.constants.ts
+++
@@ -1,7 +0,0 @@
-import { Contact } from '../../shared/interfaces/contact.interface';
-
-export const EMPTY_CONTACT: Contact = {
-    id: null,
-    forename: null,
-    surname: null,
-};
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/back-office', '<rootDir>/apps/front'],
+};

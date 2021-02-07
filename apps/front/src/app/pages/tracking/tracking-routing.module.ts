d77283dadde8398c52f76afb91e07708abc58b14
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:28:03 +0300

    Register home route

--- apps/front/src/app/pages/tracking/tracking-routing.module.ts
+++ apps/front/src/app/pages/tracking/tracking-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { TrackingComponent } from './tracking.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: TrackingComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TrackingRoutingModule { }
+export class TrackingRoutingModule {}

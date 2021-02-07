e4217590d3f9b983249b225e132a3e8d9c61a984
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:12:09 +0300

    Add aside sidebar items

--- apps/front/src/app/layout/administration/administration.constants.ts
+++ apps/front/src/app/layout/administration/administration.constants.ts
@@ -1,7 +1,9 @@
+import { NavItem } from './interfaces/nav-item.interface';
+
 export enum NavItemType {
-  MENU,
-  NAV_HEADING,
-  DIVIDER,
+    MENU,
+    NAV_HEADING,
+    DIVIDER,
 }
 
 export const SIDENAV_PINNED = 'g-sidenav-pinned';
@@ -9,33 +11,53 @@ export const SIDENAV_SHOW = 'g-sidenav-show';
 export const SIDENAV_HIDE = 'g-sidenav-hide';
 export const SIDENAV_HIDDEN = 'g-sidenav-hidden';
 export const SIDEBAR_ITEMS = [
-  {
-    label: 'Dashboard'
-  },
-  {
-    label: 'Tracking'
-  },
-  {
-    label: 'Tournois'
-  },
-  {
-    label: 'Entrainements',
-    children: [
-      {
-        label: 'Programmes'
-      },
-      {
-        label: 'Statistiques'
-      },
-      {
-        label: `Jeux d'adresse`
-      }
-    ]
-  },
-  {
-    label: 'Equipes'
-  },
-  {
-    label: 'Sponsorship'
-  }
+    {
+        label: 'Dashboard',
+        icon: 'fa fa-home',
+    },
+    {
+        label: 'Tracking',
+        icon: 'fa fa-line-chart',
+    },
+    {
+        label: 'Tournois',
+        icon: 'fa fa-trophy',
+    },
+    {
+        label: 'Entrainements',
+        icon: 'fa fa-gamepad',
+        children: [
+            {
+                label: 'Programmes',
+            },
+            {
+                label: 'Statistiques',
+            },
+            {
+                label: `Jeux d'adresse`,
+            },
+        ],
+    },
+    {
+        label: 'Equipes',
+        icon: 'fa fa-users',
+    },
+];
+export const ASIDE_SIDEBAR_ITEMS: NavItem[] = [
+    {
+        label: 'Sponsorship',
+        icon: 'fa fa-university',
+    },
+    {
+        label: 'Shop',
+        icon: 'fa fa-shopping-cart',
+    },
+    {
+        label: 'FAQ',
+        icon: 'fa fa-question-circle',
+    },
+    {
+        label: 'Contacte-nous',
+        icon: 'fa fa-envelope',
+    },
 ];

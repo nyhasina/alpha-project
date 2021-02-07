3982322f5cb6afd599b1e7b5fff2e8ba6e095057
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 19:27:26 +0300

    Delete type property

--- apps/front/src/app/layout/administration/interfaces/nav-item.interface.ts
+++ apps/front/src/app/layout/administration/interfaces/nav-item.interface.ts
@@ -1,9 +1,5 @@
-import { NavItemType } from '../administration.constants';
-
 export interface NavItem {
-  type: NavItemType;
   label?: string;
-  abbreviation?: string;
   icon?: string;
   children?: NavItem[];
   routerLink?: string | string[];

2752c8ced578ae92e8ac353a534f16f1a52c7502
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:48:01 +0300

    Register training routes

--- apps/front/src/app/pages/training/training-routing.module.ts
+++ apps/front/src/app/pages/training/training-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProgramRootComponent } from './containers/program-root/program-root.component';
+import { SkillGamesRootComponent } from './containers/skill-games-root/skill-games-root.component';
+import { StatisticRootComponent } from './containers/statistic-root/statistic-root.component';
 
 const routes: Routes = [
+    {
+        path: 'program',
+        component: ProgramRootComponent,
+    },
+    {
+        path: 'skill-games',
+        component: SkillGamesRootComponent,
+    },
+    {
+        path: 'statistic',
+        component: StatisticRootComponent,
+    },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class TrainingRoutingModule { }
+export class TrainingRoutingModule {}

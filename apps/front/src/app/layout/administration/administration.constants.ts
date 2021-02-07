276d488327e1827becdf7211ff29f35259fcb286
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 20:20:12 +0300

    Move all constants to this file

--- apps/front/src/app/layout/administration/administration.constants.ts
+++ apps/front/src/app/layout/administration/administration.constants.ts
@@ -3,3 +3,39 @@ export enum NavItemType {
   NAV_HEADING,
   DIVIDER,
 }
+
+export const SIDENAV_PINNED = 'g-sidenav-pinned';
+export const SIDENAV_SHOW = 'g-sidenav-show';
+export const SIDENAV_HIDE = 'g-sidenav-hide';
+export const SIDENAV_HIDDEN = 'g-sidenav-hidden';
+export const SIDEBAR_ITEMS = [
+  {
+    label: 'Dashboard'
+  },
+  {
+    label: 'Tracking'
+  },
+  {
+    label: 'Tournois'
+  },
+  {
+    label: 'Entrainements',
+    children: [
+      {
+        label: 'Programmes'
+      },
+      {
+        label: 'Statistiques'
+      },
+      {
+        label: `Jeux d'adresse`
+      }
+    ]
+  },
+  {
+    label: 'Equipes'
+  },
+  {
+    label: 'Sponsorship'
+  }
+];

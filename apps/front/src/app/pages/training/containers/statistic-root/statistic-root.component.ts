a3b38b8218f59001c6346aa7091b5cc814b2252c
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:44:39 +0300

    Create statistic root

---
+++ apps/front/src/app/pages/training/containers/statistic-root/statistic-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-statistic-root',
+  templateUrl: './statistic-root.component.html',
+  styleUrls: ['./statistic-root.component.scss']
+})
+export class StatisticRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

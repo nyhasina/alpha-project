cba4640e62529cf635f1adb1c837a59e2a3f57e4
Manjato Nick <manjato25@gmail.com>
2021-02-07 20:52:00 +0300

    integration register

--- apps/front/src/app/pages/authentication-page/authentication-page.module.ts
+++ apps/front/src/app/pages/authentication-page/authentication-page.module.ts
@@ -6,9 +6,10 @@ import { LoginComponent } from './components/login/login.component';
 import { LoginRootComponent } from './containers/login-root/login-root.component';
 import { RegisterRootComponent } from './containers/register-root/register-root.component';
 import { RegisterComponent } from './components/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [LoginRootComponent, RegisterRootComponent, LoginComponent, RegisterComponent],
-    imports: [CommonModule, AuthenticationPageRoutingModule],
+    imports: [CommonModule, AuthenticationPageRoutingModule, FormsModule,ReactiveFormsModule],
 })
 export class AuthenticationPageModule {}
--- apps/front/src/app/pages/authentication-page/components/register/register.component.ts
+++ apps/front/src/app/pages/authentication-page/components/register/register.component.ts
@@ -1,14 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'nicecactus-platform-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../login/login.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
+  formRegister: FormGroup;
+  constructor(
+    private formBuilder?: FormBuilder,
+  ) {
+    this.createformRegister();
+   }
+   createformRegister() {
+    this.formRegister = this.formBuilder.group({
+      email: ['', Validators.compose([
+        Validators.required,
+        this.checkMail
+      ])],
+      username: ['', Validators.required],
+      password: ['', Validators.compose([
+        Validators.required,
+      ])],
+    });
+  }
+  checkMail(controls) {
+    const regExp = new RegExp(/\S+@\S+\.\S+/);
+    if (regExp.test(controls.value)) {
+      return null;
+    } else {
+      return { checkMail: true };
+    }
+  }
   ngOnInit(): void {
   }
 

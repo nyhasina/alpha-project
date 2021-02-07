c57cb81d0b2613253d6932be8e312a4a8ba65132
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 14:03:49 +0300

    Create login root component

---
+++ apps/front/src/app/pages/authentication-page/containers/login-root/login-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-login-root',
+  templateUrl: './login-root.component.html',
+  styleUrls: ['./login-root.component.scss']
+})
+export class LoginRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

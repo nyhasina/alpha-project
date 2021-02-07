2e7a0dc8ed302b61aeb0490c94e0da91bd9eac38
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 13:34:14 +0300

    Ident files

--- apps/front/src/app/layout/landing/header/header.component.ts
+++ apps/front/src/app/layout/landing/header/header.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // eslint-disable-next-line @angular-eslint/component-selector
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  menuListes = ['Tournaments', 'Training', 'Game', 'Community'];
-  fa = ['globe', 'shopping-bag']
-  navbarOpen = false;
-  toggleNavbar() {
-    this.navbarOpen = !this.navbarOpen;
-  }
+    menuListes = ['Tournaments', 'Training', 'Game', 'Community'];
+    fa = ['globe', 'shopping-bag'];
+    navbarOpen = false;
 
+    toggleNavbar() {
+        this.navbarOpen = !this.navbarOpen;
+    }
 }

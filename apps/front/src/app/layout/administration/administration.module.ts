bc4cc6a91804a682c7c289307caafd3ca15615ff
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 14:32:29 +0300

    Create navbar component

--- apps/front/src/app/layout/administration/administration.module.ts
+++ apps/front/src/app/layout/administration/administration.module.ts
@@ -6,9 +6,10 @@ import { AdministrationComponent } from './administration.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-    declarations: [AdministrationComponent, FooterComponent, HeaderComponent, SidebarComponent],
+    declarations: [AdministrationComponent, FooterComponent, HeaderComponent, SidebarComponent, NavbarComponent],
     imports: [CommonModule, AdministrationRoutingModule],
 })
 export class AdministrationModule {}

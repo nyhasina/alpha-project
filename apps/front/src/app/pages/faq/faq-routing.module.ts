b8864db859135cddcb9d449779b32ba3595dde1c
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:55:35 +0300

    Create faq page

--- apps/front/src/app/pages/faq/faq-routing.module.ts
+++ apps/front/src/app/pages/faq/faq-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FaqComponent } from './faq.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: FaqComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class FaqRoutingModule { }
+export class FaqRoutingModule {}

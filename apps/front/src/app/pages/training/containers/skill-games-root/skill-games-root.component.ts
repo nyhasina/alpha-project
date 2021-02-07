72fa45e56ed11c57ed837033b9848f7d608da53a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-07 21:44:55 +0300

    Create skill games root

---
+++ apps/front/src/app/pages/training/containers/skill-games-root/skill-games-root.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-skill-games-root',
+  templateUrl: './skill-games-root.component.html',
+  styleUrls: ['./skill-games-root.component.scss']
+})
+export class SkillGamesRootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

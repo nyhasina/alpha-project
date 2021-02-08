e7cc06f496d11f99b0a9d6869fb6eac17c7847f3
manjato <manjato25@gmail.com>
2021-02-08 17:49:10 +0300

    create component games

--- apps/front/src/app/layout/landing/landing.module.ts
+++ apps/front/src/app/layout/landing/landing.module.ts
@@ -5,9 +5,10 @@ import { LandingRoutingModule } from './landing-routing.module';
 import { LandingComponent } from './landing.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
-    declarations: [LandingComponent, HeaderComponent, FooterComponent],
+    declarations: [LandingComponent, HeaderComponent, FooterComponent, GamesComponent],
     imports: [CommonModule, LandingRoutingModule],
 })
 export class LandingModule {}

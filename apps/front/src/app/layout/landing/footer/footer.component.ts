913e571e146db92d4624a9ff87eea32946fdfef2
manjato <manjato25@gmail.com>
2021-02-08 17:47:06 +0300

    integration aboutus

--- apps/front/src/app/layout/landing/footer/footer.component.ts
+++ apps/front/src/app/layout/landing/footer/footer.component.ts
@@ -6,7 +6,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-  footerListes:Array<string> =['Avis Juridique', 'Conditions d\'utilisation',  'Conditions de vente', 'Politique de confidentialité et de cookies']
+  aboutUsListes: Array<any> = [
+    {
+      fa:'fa-traffic-light',
+      detail: '2000',
+      name:'TOURNOIS PAR MOIS'
+    },
+    {
+      fa:'fa-award',
+      detail: 'de l\'Europe',
+      name:'LA PLATEFORME "TOUT EN UN" QUI SE DÉVELOPPE LE PLUS RAPIDEMENT,'
+    },
+    {
+      fa:'fa-users',
+      detail: '1 364 254',
+      name:'COMMUNAUTÉ ET CROISSANCE'
+    },
+    {
+      fa:'fa-road',
+      detail: 'Road to sponsorship',
+      name:''
+    },
+    {
+      fa:'fa-award',
+      detail: '1 million',
+      name:'en cash prize'
+    }
+  ]
+  footerListes: Array<string> = ['Avis Juridique', 'Conditions d\'utilisation',  'Conditions de vente', 'Politique de confidentialité et de cookies'];
+
   constructor() { }
 
   ngOnInit(): void {

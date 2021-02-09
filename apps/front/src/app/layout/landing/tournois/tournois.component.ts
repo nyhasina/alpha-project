6dabbd0b3b311acaf59b00a1c15998e7eacfc685
manjato <manjato25@gmail.com>
2021-02-09 16:43:19 +0300

    integration tournois

--- apps/front/src/app/layout/landing/tournois/tournois.component.ts
+++ apps/front/src/app/layout/landing/tournois/tournois.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournois.component.scss']
 })
 export class TournoisComponent implements OnInit {
-
+gamesList: Array<string> = ['Apex legends', 'Auto Chess', 'Brawl Stars', 'Brawlhalla', 'Call Of Duty Cold War', 'Call of duty Mobile', 'Call of duty Modern warfare', ' Clash royal', 'FIFA 21', 'Fortnite', 'Hearthstone', 'Legends of Runeterra', 'LOL', 'Mario kart tour', 'Rocket League', 'Rogue Company', 'Super Smash Bros. Ultimate', 'TFT', 'Tom Clancy\'s Rainbow Six: Siege', 'Valorant', ' Wild Rift']
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  trackByFn(index, item) {
+    return item.id;
+  }
 }

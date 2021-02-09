c4b510ce3c5a926cfcf26b835df38d6c533cb035
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-09 22:53:33 +0300

    Create authentication page

---
+++ apps/back-office/src/app/pages/authentication-page/components/authentication-form/authentication-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-authentication-form',
+  templateUrl: './authentication-form.component.html',
+  styleUrls: ['./authentication-form.component.scss']
+})
+export class AuthenticationFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ apps/back-office/src/app/pages/authentication-page/components/authentication-header/authentication-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-authentication-header',
+  templateUrl: './authentication-header.component.html',
+  styleUrls: ['./authentication-header.component.scss']
+})
+export class AuthenticationHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

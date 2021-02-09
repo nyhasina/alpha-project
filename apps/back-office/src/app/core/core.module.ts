3a89ac9409e53a371dd6d6cc06261cf68ad68c96
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-09 21:54:23 +0300

    Scaffold core module

--- apps/back-office/src/app/core/core.module.ts
+++ apps/back-office/src/app/core/core.module.ts
@@ -1,12 +1,25 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../../environments/environment';
+import { CoreEffects } from './store/core.effects';
+import { reducers } from './store/core.reducer';
+import { CoreRouterEffects } from './store/core.router-effects';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        StoreModule.forRoot(reducers),
+        StoreDevtoolsModule.instrument({
+            maxAge: 50,
+            logOnly: environment.production,
+        }),
+        EffectsModule.forRoot([CoreEffects, CoreRouterEffects]),
+        StoreRouterConnectingModule.forRoot(),
+    ],
 })
-export class CoreModule { }
+export class CoreModule {}

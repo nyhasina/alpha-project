d7548c34fd58b76a1efb58b0d373ba6ff02f77fc
Manjato Nick <manjato25@gmail.com>
2021-02-10 02:49:23 +0300

    integration tournois component

--- apps/front/src/app/layout/landing/tournois/tournois.component.ts
+++ apps/front/src/app/layout/landing/tournois/tournois.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournois.component.scss']
 })
 export class TournoisComponent implements OnInit {
-gamesList: Array<string> = ['Apex legends', 'Auto Chess', 'Brawl Stars', 'Brawlhalla', 'Call Of Duty Cold War', 'Call of duty Mobile', 'Call of duty Modern warfare', ' Clash royal', 'FIFA 21', 'Fortnite', 'Hearthstone', 'Legends of Runeterra', 'LOL', 'Mario kart tour', 'Rocket League', 'Rogue Company', 'Super Smash Bros. Ultimate', 'TFT', 'Tom Clancy\'s Rainbow Six: Siege', 'Valorant', ' Wild Rift']
+// eslint-disable-next-line max-len
+gamesList: Array<string> = ['Apex legends', 'Auto Chess', 'Brawl Stars', 'Brawlhalla', 'Call Of Duty Cold War', 'Call of duty Mobile', 'Call of duty Modern warfare', ' Clash royal', 'FIFA 21', 'Fortnite', 'Hearthstone', 'Legends of Runeterra', 'LOL', 'Mario kart tour', 'Rocket League', 'Rogue Company', 'Super Smash Bros. Ultimate', 'TFT', 'Tom Clancy\'s Rainbow Six: Siege', 'Valorant', ' Wild Rift'];
+gameCalendar: Array<any> = [
+  {
+    name: 'Apex Legends Top Dégâts',
+    logo:'assets/img/brand/ps.png',
+    prize: '3£',
+    date: '02/09/2021, 9:44 PM'
+  },
+  {
+    name: 'Apex Legends Top Dégâts',
+    logo:'assets/img/brand/ps.png',
+    prize: '3£',
+    date: '02/09/2021, 9:44 PM'
+  },
+  {
+    name: 'Apex Legends Top Dégâts',
+    logo:'assets/img/brand/ps.png',
+    prize: '3£',
+    date: '02/09/2021, 9:44 PM'
+  }
+]
   constructor() { }
 
   ngOnInit(): void {

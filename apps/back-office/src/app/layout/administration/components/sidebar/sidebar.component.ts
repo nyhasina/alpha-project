74eaa41d3270b6b32ae4f315d4953d44a233da84
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-09 23:10:21 +0300

    Move sidebar in a separate component

---
+++ apps/back-office/src/app/layout/administration/components/sidebar/sidebar.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavItem } from '../../interfaces/nav-item.interface';
+
+@Component({
+    selector: 'nicecactus-platform-sidebar',
+    templateUrl: './sidebar.component.html',
+    styleUrls: ['./sidebar.component.scss'],
+})
+export class SidebarComponent {
+    @Input() sidebarItems: NavItem[];
+    @Input() asideSidebarItems: NavItem[];
+
+    constructor(private router: Router) {}
+
+    go(routerLink: string[]) {
+      if (!routerLink) {
+        return;
+      }
+      this.router.navigate(routerLink);
+    }
+}

c1c587048bd5f687ff9c8653f59d0e0d7765807f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-09 22:40:20 +0300

    Fix eslint error

--- apps/back-office/src/app/app.component.ts
+++ apps/back-office/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'nicecactus-platform-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    // eslint-disable-next-line @angular-eslint/component-selector
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'back-office';
+    title = 'ng-nicecactus';
 }

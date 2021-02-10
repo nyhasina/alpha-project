0a5b4b97fa1f16ffba8f759221cf1e073b8cdbab
Manjato Nick <manjato25@gmail.com>
2021-02-10 22:13:10 +0300

    scroll To element

--- apps/front/src/app/layout/landing/components/header/header.component.ts
+++ apps/front/src/app/layout/landing/components/header/header.component.ts
@@ -10,12 +10,21 @@ export class HeaderComponent {
     menuListes = ['Tournaments', 'Training', 'Game', 'Community'];
     fa = ['globe', 'shopping-bag'];
     navbarOpen = false;
-
+    idElement: string;
     toggleNavbar() {
         this.navbarOpen = !this.navbarOpen;
     }
     scrollTo(section: string) {
-        document.getElementById(section)
+        if (section === 'Tournaments') {
+            this.idElement = 'tournois'
+        } else if (section === 'Training') {
+            this.idElement = 'training'
+        } else if (section === 'Game') {
+            this.idElement = 'games'
+        }else if (section === 'Community') {
+            this.idElement = 'community'
+        } 
+        document.getElementById(this.idElement)
           .scrollIntoView({ behavior: 'smooth', block: 'start' });
       }
 }

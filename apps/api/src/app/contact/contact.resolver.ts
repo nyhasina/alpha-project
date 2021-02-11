8474634ee9d524a71944940a804b64961ddbc1de
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 15:22:23 +0300

    Create contact resolver and implement load contact query

--- apps/api/src/app/contact/contact.resolver.ts
+++ apps/api/src/app/contact/contact.resolver.ts
@@ -1,4 +1,13 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { ContactModel } from './contact.model';
+import { ContactService } from './contact.service';
 
-@Resolver()
-export class ContactResolver {}
+@Resolver((of) => ContactModel)
+export class ContactResolver {
+    constructor(private contactService: ContactService) {}
+
+    @Query((returns) => ContactModel)
+    async loadContact(@Args('id', { type: () => Int }) id: number) {
+        return this.contactService.loadContact({ id });
+    }
+}

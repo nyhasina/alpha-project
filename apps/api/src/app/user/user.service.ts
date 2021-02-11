a0a948569de94beddf4215aeb86ec6ac7a8e6191
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 00:40:43 +0300

    Ident file correctly

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -5,52 +5,53 @@ import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {
+  }
 
-    async createUser(data: Prisma.UserCreateInput): Promise<User> {
-        const { password } = data;
-        const hash = await this.encrypt(password);
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const { password } = data;
+    const hash = await this.encrypt(password);
 
-        return this.prisma.user.create({ data: { ...data, password: hash } });
-    }
+    return this.prisma.user.create({ data: { ...data, password: hash } });
+  }
 
-    async loadUsers(params: {
-        skip?: number;
-        take?: number;
-        cursor?: Prisma.UserWhereUniqueInput;
-        where?: Prisma.UserWhereInput;
-        orderBy?: Prisma.UserOrderByInput;
-    }): Promise<User[]> {
-        const { skip, take, cursor, where, orderBy } = params;
-        return this.prisma.user.findMany({
-            skip,
-            take,
-            cursor,
-            where,
-            orderBy,
-        });
-    }
+  async loadUsers(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.UserWhereUniqueInput;
+    where?: Prisma.UserWhereInput;
+    orderBy?: Prisma.UserOrderByInput;
+  }): Promise<User[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.user.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy
+    });
+  }
 
-    async encrypt(password: string) {
-        const saltOrRound = 10;
-        return await bcrypt.hash(password, saltOrRound);
-    }
+  async encrypt(password: string) {
+    const saltOrRound = 10;
+    return await bcrypt.hash(password, saltOrRound);
+  }
 
-    async loadUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-        return this.prisma.user.findUnique({ where });
-    }
+  async loadUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.findUnique({ where });
+  }
 
-    async updateUser(params: { where: Prisma.UserWhereUniqueInput; data: Prisma.UserUpdateInput }): Promise<User> {
-        const { where, data } = params;
-        return this.prisma.user.update({
-            data,
-            where,
-        });
-    }
+  async updateUser(params: { where: Prisma.UserWhereUniqueInput; data: Prisma.UserUpdateInput }): Promise<User> {
+    const { where, data } = params;
+    return this.prisma.user.update({
+      data,
+      where
+    });
+  }
 
-    async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
-        return this.prisma.user.delete({
-            where,
-        });
-    }
+  async deleteUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({
+      where
+    });
+  }
 }

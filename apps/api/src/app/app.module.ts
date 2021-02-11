ccb743193ed1630b64b80c026004352d9cd7fe66
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 19:48:57 +0300

    Remove unsused files

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,20 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot({
-            autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
-            include: [ContactModule],
-        }),
-        ContactModule,
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'graphql/schema.gql')
+    }),
+    ContactModule
+  ]
 })
-export class AppModule {}
+export class AppModule {
+}

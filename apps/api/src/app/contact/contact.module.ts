fe2fe3bf308e6b6284381d766eb7aefa3f7d6445
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 15:21:51 +0300

    Register prisma service, contact service and resolver

--- apps/api/src/app/contact/contact.module.ts
+++ apps/api/src/app/contact/contact.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ContactResolver } from './contact.resolver';
 import { ContactService } from './contact.service';
 
 @Module({
-    providers: [ContactService],
+    providers: [PrismaService, ContactResolver, ContactService],
 })
 export class ContactModule {}

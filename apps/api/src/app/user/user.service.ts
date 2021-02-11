acc72faf277b0eae75018b5f8155fa143aee2c83
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 21:53:05 +0300

    Install bcryp package

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prisma: PrismaService) {}
+
+    async createUser(data: Prisma.UserCreateInput): Promise<User> {
+        const { password } = data;
+        return this.prisma.user.create({ data });
+    }
+}

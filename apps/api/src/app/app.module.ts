e5f186c939ac858aa2a08faf61d483e4341f7bc6
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 15:21:27 +0300

    Register graphql module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-    imports: [],
+    imports: [
+        GraphQLModule.forRoot({
+            autoSchemaFile: join(process.cwd(), 'graphql/schema.gql'),
+            include: [ContactModule],
+        }),
+        ContactModule,
+    ],
     controllers: [AppController],
-    providers: [AppService, PrismaService],
+    providers: [AppService],
 })
 export class AppModule {}

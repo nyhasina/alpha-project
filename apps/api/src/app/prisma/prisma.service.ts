f3106d9993a4c00985237384ff2f370664bf3cef
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 11:53:48 +0300

    Wrap prisma client inside prisma service

--- apps/api/src/app/prisma/prisma.service.ts
+++ apps/api/src/app/prisma/prisma.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from 'prisma/prisma-client/scripts/default-index';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor() {
+        super();
+    }
+
+    async onModuleInit(): Promise<void> {
+        await this.$connect();
+    }
+
+    async onModuleDestroy(): Promise<void> {
+        await this.$disconnect();
+    }
+}

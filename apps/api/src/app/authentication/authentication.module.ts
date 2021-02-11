2818fd6a0ff1834fc25d362804e86d1ff53ed896
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 21:22:45 +0300

    Create authentication service

--- apps/api/src/app/authentication/authentication.module.ts
+++ apps/api/src/app/authentication/authentication.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
 
-@Module({})
+@Module({
+  providers: [AuthenticationService]
+})
 export class AuthenticationModule {}

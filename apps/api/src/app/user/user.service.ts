ba788b1f124106309487f66c62c63b699acb4182
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 22:19:07 +0300

    Implement create, load user and validate user credential functionnality

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,6 +1,8 @@
+import { parseJsonSchemaToSubCommandDescription } from '@angular/cli/utilities/json-schema';
 import { Injectable } from '@nestjs/common';
 import { Prisma, User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -8,6 +10,26 @@ export class UserService {
 
     async createUser(data: Prisma.UserCreateInput): Promise<User> {
         const { password } = data;
-        return this.prisma.user.create({ data });
+        const hash = await this.hashPassword(password);
+
+        return this.prisma.user.create({ data: { ...data, password: hash } });
+    }
+
+    private async hashPassword(password) {
+        const saltOrRound = 10;
+        const hash = await bcrypt.hash(password, saltOrRound);
+        return hash;
+    }
+
+    async loadUser(where: Prisma.UserWhereUniqueInput): Promise<User> {
+        return this.prisma.user.findUnique({ where });
+    }
+
+    async validateCredentials(email: string, password: string): Promise<User | null> {
+        const hash = await this.hashPassword(password);
+        return this.prisma.user.findUnique({
+            email,
+            password: hash,
+        });
     }
 }

3ec2675d9ed56b5f6e4aa6f274496a073f310e3e
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-11 22:29:54 +0300

    Implement valdiate user credential method

--- apps/api/src/app/authentication/authentication.service.ts
+++ apps/api/src/app/authentication/authentication.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+    constructor(private userService: UserService) {}
+
+    async validateUserCredential(email: string, password: string): Promise<User | null> {
+        const user: User = await this.userService.loadUser({ email });
+        const hash = await this.userService.hashPassword(password);
+        if (user && user.password === hash) {
+            const { password, ...rest } = user;
+            return rest;
+        }
+        return null;
+    }
+}

043707050916962971d5bcd0de01534e2b59d031
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 21:28:58 +0300

    Create sign in reducers

--- apps/back-office/src/app/core/store/core.reducer.ts
+++ apps/back-office/src/app/core/store/core.reducer.ts
@@ -1,10 +1,37 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { routerReducer } from '@ngrx/router-store';
 import * as fromRouter from '@ngrx/router-store';
+import { createReducer, on } from '@ngrx/store';
+import { signIn, signInFail, signInSuccess } from './core.actions';
+
+export interface AuthState {
+    accessToken: string;
+    refreshToken: string;
+    signing: boolean;
+    signed: boolean;
+    signingError: HttpErrorResponse;
+}
 
 export interface AppState {
     router: fromRouter.RouterReducerState<any>;
 }
 
+const initialAuthState: AuthState = {
+    accessToken: null,
+    refreshToken: null,
+    signing: false,
+    signed: false,
+    signingError: null,
+};
+
+const authenticationReducer = createReducer(
+    initialAuthState,
+    on(signIn, (state) => ({ ...state, signing: true })),
+    on(signInFail, (state, { error }) => ({ ...state, signing: false, signed: false, signingError: error })),
+    on(signInSuccess, (state, { refreshToken, accessToken }) => ({ ...state, accessToken, refreshToken }))
+);
+
 export const reducers = {
     router: routerReducer,
+    auth: authenticationReducer,
 };

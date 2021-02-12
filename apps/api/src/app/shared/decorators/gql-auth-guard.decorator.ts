1daa10b68ef9cc6a290741e4dc76071f0a9bf95d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 10:44:46 +0300

    Override can activate guard

--- apps/api/src/app/shared/decorators/gql-auth-guard.decorator.ts
+++ apps/api/src/app/shared/decorators/gql-auth-guard.decorator.ts
@@ -1,10 +1,27 @@
+import { Injectable } from '@angular/core';
 import { ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { GqlExecutionContext } from '@nestjs/graphql';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from './is-public.decorator';
 
+@Injectable()
 export class GqlAuthGuard extends AuthGuard('jwt') {
+    constructor(private reflector: Reflector) {
+        super();
+    }
+
     getRequest(context: ExecutionContext) {
         const ctx = GqlExecutionContext.create(context);
         return ctx.getContext().req;
+    }
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()]);
+        if (isPublic) {
+            return true;
+        }
+        return super.canActivate(context);
     }
 }

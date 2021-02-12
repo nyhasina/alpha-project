0bf5e1c392ef15fc7e442f59a76a62dbd7facf5a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 22:57:00 +0300

    Create sign out reducer

--- apps/back-office/src/app/core/store/core.reducer.ts
+++ apps/back-office/src/app/core/store/core.reducer.ts
@@ -3,7 +3,7 @@ import * as fromRouter from '@ngrx/router-store';
 import { routerReducer } from '@ngrx/router-store';
 import { ActionReducer, createReducer, MetaReducer, on } from '@ngrx/store';
 import { localStorageSync } from 'ngrx-store-localstorage';
-import { signIn, signInFail, signInSuccess } from './core.actions';
+import { signIn, signInFail, signInSuccess, signOut } from './core.actions';
 
 export interface AuthState {
     accessToken: string;
@@ -30,7 +30,15 @@ const authenticationReducer = createReducer(
     initialAuthState,
     on(signIn, (state) => ({ ...state, signing: true })),
     on(signInFail, (state, { error }) => ({ ...state, signing: false, signed: false, signingError: error })),
-    on(signInSuccess, (state, { refreshToken, accessToken }) => ({ ...state, accessToken, refreshToken }))
+    on(signInSuccess, (state, { refreshToken, accessToken }) => ({ ...state, accessToken, refreshToken })),
+    on(signOut, (state) => ({
+        ...state,
+        accessToken: null,
+        refreshToken: null,
+        signingError: null,
+        signed: false,
+        signing: false,
+    }))
 );
 
 export const reducers = {
@@ -39,7 +47,7 @@ export const reducers = {
 };
 
 export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
-  return localStorageSync({ keys: ['auth'], rehydrate: true })(reducer);
+    return localStorageSync({ keys: ['auth'], rehydrate: true })(reducer);
 }
 
 export const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];

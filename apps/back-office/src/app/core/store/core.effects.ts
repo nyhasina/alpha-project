b5eb5e8a2f1ef65107bbf50495a72a4ea42389b7
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 21:30:35 +0300

    Create navigation effects

--- apps/back-office/src/app/core/store/core.effects.ts
+++ apps/back-office/src/app/core/store/core.effects.ts
@@ -1,7 +1,53 @@
+import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Router } from '@angular/router';
+import { ApolloQueryResult } from '@apollo/client';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { AuthenticationResponse } from '@nicecactus-platform/types';
+import { of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { AuthenticationService } from '../services/authentication.service';
+import { backward, forward, go, signIn, signInFail, signInSuccess } from './core.actions';
 
 @Injectable()
 export class CoreEffects {
-    constructor(private actions$: Actions) {}
+    go$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(go),
+                tap(({ path, extras, queryParams }) => {
+                    this.router.navigate(path, {
+                        queryParams,
+                        ...extras,
+                    });
+                })
+            ),
+        { dispatch: false }
+    );
+
+    forward$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(forward),
+                tap(() => this.location.forward())
+            ),
+        { dispatch: false }
+    );
+
+    backward$ = createEffect(
+        () =>
+            this.actions$.pipe(
+                ofType(backward),
+                tap(() => this.location.back())
+            ),
+        { dispatch: false }
+    );
+
+
+    constructor(
+        private actions$: Actions,
+        private authenticationService: AuthenticationService,
+        private router: Router,
+        private location: Location
+    ) {}
 }

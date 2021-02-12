d70329059e1318fc694800effd5c53c50b63467f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 22:36:57 +0300

    Apply is authenticated guard here

--- apps/back-office/src/app/layout/layout-routing.module.ts
+++ apps/back-office/src/app/layout/layout-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IsAuthenticatedGuard } from '@nicecactus-platform/guards';
 
 const routes: Routes = [
     {
@@ -8,6 +9,7 @@ const routes: Routes = [
             {
                 path: 'admin',
                 loadChildren: () => import('./administration/administration.module').then((m) => m.AdministrationModule),
+                canActivate: [IsAuthenticatedGuard],
             },
             {
                 path: 'auth',

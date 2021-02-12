fe5b6b17c44761c9d9b6f8447b5f7f80e9b663e7
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 12:07:13 +0300

    Configure rate limit security

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -7,6 +7,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -14,6 +15,12 @@ async function bootstrap() {
     app.setGlobalPrefix(globalPrefix);
     const port = process.env.PORT || 3333;
     app.use(csurf);
+    app.use(
+        rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100, // limit each IP to 100 requests per windowMs
+        })
+    );
     await app.listen(port, () => {
         Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
     });

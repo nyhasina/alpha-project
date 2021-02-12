9b2122c43b359b683a38fcd4cb9e8bcefc955c19
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 22:34:51 +0300

    Rename guard

--- libs/guards/src/lib/guards.module.ts
+++ libs/guards/src/lib/guards.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IsAuthenticatedGuard } from './is-authenticated.guard';
 
 @NgModule({
     imports: [CommonModule],
+    providers: [IsAuthenticatedGuard],
 })
 export class GuardsModule {}

e5b7e5a1ba361bc9075e4e154a80e0508220689d
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 22:57:51 +0300

    Move layout module import

--- apps/back-office/src/app/app.module.ts
+++ apps/back-office/src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { LayoutModule } from './layout/layout.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, CoreModule, LayoutModule, NgbModule],
+    imports: [BrowserModule, AppRoutingModule, CoreModule, NgbModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- apps/back-office/src/app/core/core.module.ts
+++ apps/back-office/src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { GraphQlServiceModule } from '@nicecactus-platform/graph-ql-service';
 import { environment } from '../../environments/environment';
+import { LayoutModule } from '../layout/layout.module';
 import { CoreEffects } from './store/core.effects';
 import { metaReducers, reducers } from './store/core.reducer';
 import { CoreRouterEffects } from './store/core.router-effects';
@@ -15,6 +16,7 @@ import { CoreRouterEffects } from './store/core.router-effects';
     declarations: [],
     imports: [
         CommonModule,
+        LayoutModule,
         StoreModule.forRoot(reducers, { metaReducers }),
         StoreDevtoolsModule.instrument({
             maxAge: 50,

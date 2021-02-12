e8d46480adc68bc12e7933c2ff265a9259d3fa01
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 12:05:24 +0300

    Control csrf protection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,14 +5,15 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const globalPrefix = 'api';
     app.setGlobalPrefix(globalPrefix);
     const port = process.env.PORT || 3333;
+    app.use(csurf);
     await app.listen(port, () => {
         Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
     });

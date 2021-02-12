bfaa1d126a20ca36a10289ccdfc073db9f6c79cd
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-12 21:33:20 +0300

    Add ui sign in ui interactions

--- apps/back-office/src/app/pages/authentication-page/authentication-page.component.ts
+++ apps/back-office/src/app/pages/authentication-page/authentication-page.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { signIn } from '../../core/store/core.actions';
+import { AppState } from '../../core/store/core.reducer';
 
 @Component({
-  selector: 'nicecactus-platform-authentication-page',
-  templateUrl: './authentication-page.component.html',
-  styleUrls: ['./authentication-page.component.scss']
+    selector: 'nicecactus-platform-authentication-page',
+    templateUrl: './authentication-page.component.html',
+    styleUrls: ['./authentication-page.component.scss'],
 })
-export class AuthenticationPageComponent implements OnInit {
+export class AuthenticationPageComponent {
+    constructor(private store: Store<AppState>) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    onSignIn(payload: { email: string; password: string }) {
+        this.store.dispatch(signIn({ ...payload }));
+    }
 }
--- apps/back-office/src/app/pages/authentication-page/components/authentication-form/authentication-form.component.ts
+++ apps/back-office/src/app/pages/authentication-page/components/authentication-form/authentication-form.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'nicecactus-platform-authentication-form',
-  templateUrl: './authentication-form.component.html',
-  styleUrls: ['./authentication-form.component.scss']
+    selector: 'nicecactus-platform-authentication-form',
+    templateUrl: './authentication-form.component.html',
+    styleUrls: ['./authentication-form.component.scss'],
 })
 export class AuthenticationFormComponent implements OnInit {
+    form: FormGroup;
+    @Output() signIn: EventEmitter<{ email: string; password: string }> = new EventEmitter<{ email: string; password: string }>();
 
-  constructor() { }
+    constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.form = this.initForm();
+    }
 
+    onSubmit() {
+        if (this.form.valid) {
+            this.signIn.emit(this.form.value);
+        }
+    }
+
+    private initForm() {
+        return this.formBuilder.group({
+            email: [null, Validators.required],
+            password: [null, Validators.required],
+        });
+    }
 }

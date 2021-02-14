1a7a9f73c9e34ccc8b08fc6dc504db585ca10167
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-14 20:14:29 +0300

    Create platform root component

--- apps/back-office/src/app/pages/platform/platform.module.ts
+++ apps/back-office/src/app/pages/platform/platform.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlatformRoutingModule } from './platform-routing.module';
+import { PlatformComponent } from './platform.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PlatformComponent],
   imports: [
     CommonModule,
     PlatformRoutingModule

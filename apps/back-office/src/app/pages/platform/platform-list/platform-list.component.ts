94f2ff60bc31a1b42c0e4a0c28475539d2a61e59
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-14 20:15:45 +0300

    Create platform list component

---
+++ apps/back-office/src/app/pages/platform/platform-list/platform-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-platform-list',
+  templateUrl: './platform-list.component.html',
+  styleUrls: ['./platform-list.component.scss']
+})
+export class PlatformListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/back-office/src/app/pages/platform/platform.module.ts
+++ apps/back-office/src/app/pages/platform/platform.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { PlatformRoutingModule } from './platform-routing.module';
 import { PlatformComponent } from './platform.component';
 import { PlatformFormComponent } from './platform-form/platform-form.component';
+import { PlatformListComponent } from './platform-list/platform-list.component';
 
 
 @NgModule({
-  declarations: [PlatformComponent, PlatformFormComponent],
+  declarations: [PlatformComponent, PlatformFormComponent, PlatformListComponent],
   imports: [
     CommonModule,
     PlatformRoutingModule

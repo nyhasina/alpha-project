ac62f139b76dbc7ce7679ccaecbb21218bb0d35f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-14 16:00:01 +0300

    Implement platform resolver

--- apps/api/src/app/platform/platform.resolver.ts
+++ apps/api/src/app/platform/platform.resolver.ts
@@ -1,4 +1,56 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, ArgsType, Field, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { PlatformModel } from '../platform/platform.model';
+import { PlatformService } from './platform.service';
 
-@Resolver()
-export class PlatformResolver {}
+@ArgsType()
+export class CreatePlatformInput {
+    @Field()
+    name: string;
+
+    @Field({ nullable: true })
+    logo?: string;
+
+    @Field({ nullable: true })
+    deleted?: boolean;
+}
+
+@Resolver((of) => PlatformModel)
+export class PlatformResolver {
+    constructor(private platformService: PlatformService) {}
+    @Query((returns) => PlatformModel)
+    async platform(@Args('id', { type: () => Int, name: 'platform' }) id: number) {
+        return this.platformService.loadPlatform({ id });
+    }
+
+    @Query((returns) => [PlatformModel])
+    async platforms() {
+        return this.platformService.loadPlatforms({});
+    }
+
+    @Mutation((returns) => PlatformModel)
+    async createPlatform(@Args() input: CreatePlatformInput) {
+        const { name, logo } = input;
+        return this.platformService.createPlatform({ name, logo });
+    }
+
+    @Mutation((returns) => PlatformModel)
+    async updatePlatform(@Args('id', { type: () => Int }) id: number, @Args() input: CreatePlatformInput) {
+        const { name, logo } = input;
+        return this.platformService.updatePlatform({
+            where: {
+                id,
+            },
+            data: {
+                name,
+                logo,
+            },
+        });
+    }
+
+    @Mutation((returns) => PlatformModel)
+    async deletePlatform(@Args('id', { type: () => Int }) id: number) {
+        return this.platformService.deletePlatform({
+            id,
+        });
+    }
+}

cf5e5cd4cdcf88961a77d8f478c235108d96e6b8
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-14 15:53:49 +0300

    Create platform resolver

--- apps/api/src/app/platform/platform.module.ts
+++ apps/api/src/app/platform/platform.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PlatformService } from './platform.service';
+import { PlatformResolver } from './platform.resolver';
 
 @Module({
-  providers: [PlatformService]
+  providers: [PlatformService, PlatformResolver]
 })
 export class PlatformModule {}

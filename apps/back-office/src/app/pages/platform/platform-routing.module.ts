c740a3978f8999f97e8dd4e07c36e0fff7db322f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-15 16:46:53 +0300

    Edit platform module routes

--- apps/back-office/src/app/pages/platform/platform-routing.module.ts
+++ apps/back-office/src/app/pages/platform/platform-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PlatformComponent } from './platform.component';
+import { PlatformFormRootComponent } from './containers/platform-form-root/platform-form-root.component';
+import { PlatformListRootComponent } from './containers/platform-list-root/platform-list-root.component';
 
 const routes: Routes = [
     {
         path: '',
-        component: PlatformComponent,
+        component: PlatformListRootComponent,
+        children: [
+            {
+                path: 'edit/:platformId',
+                component: PlatformFormRootComponent,
+            },
+            {
+                path: 'new',
+                component: PlatformFormRootComponent,
+            },
+        ],
     },
 ];
 

5c25ee8233db4d89227bc29d43a75578fb696737
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-15 16:44:31 +0300

    Separate list and form root

---
+++ apps/back-office/src/app/pages/platform/containers/platform-list-root/platform-list-root.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Platform } from '@nicecactus-platform/graph-ql-service';
+import { Observable } from 'rxjs';
+import { go } from '../../../../core/store/core.actions';
+import { AppState } from '../../../../core/store/core.reducer';
+import { savePlatform } from '../../store/platform.actions';
+import { PlatformState } from '../../store/platform.reducers';
+import { selectPlatform, selectPlatforms } from '../../store/platform.selectors';
+
+@Component({
+    selector: 'nicecactus-platform-platform',
+    templateUrl: './platform-list-root.component.html',
+    styleUrls: ['./platform-list-root.component.scss'],
+})
+export class PlatformListRootComponent implements OnInit {
+    platforms$: Observable<Platform[]>;
+
+    constructor(private platformStore: Store<PlatformState>, private coreStore: Store<AppState>) {}
+
+    ngOnInit() {
+        this.platforms$ = this.platformStore.pipe(select(selectPlatforms));
+    }
+
+    onAdd() {
+        this.coreStore.dispatch(go({ path: ['/', 'admin', 'platform', 'new'] }));
+    }
+}

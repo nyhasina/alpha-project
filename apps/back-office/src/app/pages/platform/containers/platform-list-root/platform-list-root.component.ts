f74f2a7ff231fcd6e0ae53f93bb246930ffc0fcc
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-15 21:04:54 +0300

    Implement platform deletion business logic

--- apps/back-office/src/app/pages/platform/containers/platform-list-root/platform-list-root.component.ts
+++ apps/back-office/src/app/pages/platform/containers/platform-list-root/platform-list-root.component.ts
@@ -2,23 +2,26 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Platform } from '@nicecactus-platform/graph-ql-service';
 import { Observable } from 'rxjs';
-import { go } from '../../../../core/store/core.actions';
-import { AppState } from '../../../../core/store/core.reducer';
-import { loadPlatform, savePlatform } from '../../store/platform.actions';
+import { deletePlatform } from '../../store/platform.actions';
 import { PlatformState } from '../../store/platform.reducers';
-import { selectPlatform, selectPlatforms } from '../../store/platform.selectors';
+import { selectPlatforms } from '../../store/platform.selectors';
 
 @Component({
-    selector: 'nicecactus-platform-platform',
-    templateUrl: './platform-list-root.component.html',
-    styleUrls: ['./platform-list-root.component.scss'],
+  selector: 'nicecactus-platform-platform',
+  templateUrl: './platform-list-root.component.html',
+  styleUrls: ['./platform-list-root.component.scss']
 })
 export class PlatformListRootComponent implements OnInit {
-    platforms$: Observable<Platform[]>;
+  platforms$: Observable<Platform[]>;
 
-    constructor(private platformStore: Store<PlatformState>) {}
+  constructor(private platformStore: Store<PlatformState>) {
+  }
 
-    ngOnInit() {
-        this.platforms$ = this.platformStore.pipe(select(selectPlatforms));
-    }
+  ngOnInit() {
+    this.platforms$ = this.platformStore.pipe(select(selectPlatforms));
+  }
+
+  onDelete(id: number) {
+    this.platformStore.dispatch(deletePlatform({ id }));
+  }
 }

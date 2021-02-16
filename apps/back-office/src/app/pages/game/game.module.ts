623a55c389e2e2c2bec68f2f12ce37ec0135183f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 20:51:05 +0300

    Register all new components, routing and store module

---
+++ apps/back-office/src/app/pages/game/game.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+
+import { GameRoutingModule } from './game-routing.module';
+import { GameListRootComponent } from './containers/game-list-root/game-list-root.component';
+import { GameFormComponent } from './components/game-form/game-form.component';
+import { GameListComponent } from './components/game-list/game-list.component';
+import { GameEffects } from './store/game.effects';
+import { gameReducer } from './store/game.reducers';
+import { GameRouterEffects } from './store/game.router-effects';
+import { GameFormRootComponent } from './containers/game-form-root/game-form-root.component';
+
+@NgModule({
+    declarations: [GameListRootComponent, GameFormComponent, GameListComponent, GameFormRootComponent],
+  imports: [
+    CommonModule,
+    GameRoutingModule,
+    StoreModule.forFeature('game', gameReducer),
+    EffectsModule.forFeature([GameEffects, GameRouterEffects]),
+    ReactiveFormsModule
+  ]
+})
+export class GameModule {}

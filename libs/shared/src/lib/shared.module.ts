84a81dc65e942ed7e537b925138f5ff958947ed2
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 23:11:09 +0300

    Import pipes module and ng select module

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { PipesModule } from '@nicecactus-platform/pipes';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, PipesModule, NgSelectModule],
     declarations: [PaginatorComponent, SearchBarComponent],
-    exports: [PaginatorComponent, SearchBarComponent],
+    exports: [PaginatorComponent, SearchBarComponent, PipesModule],
 })
 export class SharedModule {}

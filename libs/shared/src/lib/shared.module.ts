9ec44be7fa8a0e1a87e681e4fc90e149a4d4fbc5
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 23:02:39 +0300

    Create search bar component

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginatorComponent } from './components/paginator/paginator.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
     imports: [CommonModule],
-    declarations: [PaginatorComponent],
+    declarations: [PaginatorComponent, SearchBarComponent],
 })
 export class SharedModule {}

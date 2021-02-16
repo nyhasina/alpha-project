106cfa138f94640d4a011ffd5b3a1f6715ef9395
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 22:11:13 +0300

    Register object to string pipe

--- libs/pipes/src/index.ts
+++ libs/pipes/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/pipes.module';
+export * from './lib/objet-to-string.pipe';
--- libs/pipes/src/lib/pipes.module.ts
+++ libs/pipes/src/lib/pipes.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FirstLetterPipe } from './first-letter.pipe';
+import { ObjetToStringPipe } from './objet-to-string.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FirstLetterPipe],
-  exports: [FirstLetterPipe]
+  declarations: [FirstLetterPipe, ObjetToStringPipe],
+  exports: [FirstLetterPipe, ObjetToStringPipe]
 })
 export class PipesModule {
 }

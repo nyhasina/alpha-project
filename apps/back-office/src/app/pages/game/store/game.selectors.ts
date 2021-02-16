1f25a7aabf9f6d33ec794db7553de3801116a74f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 20:47:51 +0300

    Create game selectors

---
+++ apps/back-office/src/app/pages/game/store/game.selectors.ts
@@ -0,0 +1,18 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { GameState } from './game.reducers';
+
+export const selectGameState = createFeatureSelector<GameState>('game');
+export const selectGames = createSelector(selectGameState, (state: GameState) => state.games);
+export const selectLoadingGames = createSelector(selectGameState, (state: GameState) => state.loadingGames);
+export const selectGamesLoaded = createSelector(selectGameState, (state: GameState) => state.gamesLoaded);
+export const selectLoadingGamesError = createSelector(
+    selectGameState,
+    (state: GameState) => state.loadingGamesError
+);
+export const selectGame = createSelector(selectGameState, (state: GameState) => state.game);
+export const selectLoadingGame = createSelector(selectGameState, (state: GameState) => state.loadingGame);
+export const selectGameLoaded = createSelector(selectGameState, (state: GameState) => state.gameLoaded);
+export const selectLoadingGameError = createSelector(
+    selectGameState,
+    (state: GameState) => state.loadingGameError
+);

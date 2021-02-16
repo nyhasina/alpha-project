a10072984da39792eb062aa25c9aaee83ad0d859
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 20:47:01 +0300

    Create game actions

---
+++ apps/back-office/src/app/pages/game/store/game.actions.ts
@@ -0,0 +1,19 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { createAction, props } from '@ngrx/store';
+import { Game } from '@nicecactus-platform/graph-ql-service';
+
+export const createGame = createAction('[Game] Create game');
+export const createGameFail = createAction('[Game] Create game fail', props<{ error: HttpErrorResponse }>());
+export const createGameSuccess = createAction('[Game] Create game success', props<{ game: Game }>());
+export const loadGames = createAction('[Game] Load games');
+export const loadGamesFail = createAction('[Game] Load games fail', props<{ error: HttpErrorResponse }>());
+export const loadGamesSuccess = createAction('[Game] Load games success', props<{ games: Game[] }>());
+export const loadGame = createAction('[Game] Load game', props<{ id: number }>());
+export const loadGameFail = createAction('[Game] Load game fail', props<{ error: HttpErrorResponse }>());
+export const loadGameSuccess = createAction('[Game] Load game success', props<{ game: Game }>());
+export const saveGame = createAction('[Game] Save game', props<{ game: Game }>());
+export const saveGameFail = createAction('[Game] Save game fail', props<{ error: HttpErrorResponse }>());
+export const saveGameSuccess = createAction('[Game] Save game success', props<{ game: Game }>());
+export const deleteGame = createAction('[Game] Delete game', props<{ id: number }>());
+export const deleteGameFail = createAction('[Game] Delete game fail', props<{ error: HttpErrorResponse }>());
+export const deleteGameSuccess = createAction('[Game] Delete game success', props<{ game: Game }>());

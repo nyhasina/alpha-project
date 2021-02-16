0dccb2dbbfad9243685a9663a264ab6389b57787
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-16 23:01:59 +0300

    Create paginator component

---
+++ libs/shared/src/lib/components/paginator/paginator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-paginator',
+  templateUrl: './paginator.component.html',
+  styleUrls: ['./paginator.component.scss']
+})
+export class PaginatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PaginatorComponent } from './components/paginator/paginator.component';
 
 @NgModule({
     imports: [CommonModule],
+    declarations: [PaginatorComponent],
 })
 export class SharedModule {}

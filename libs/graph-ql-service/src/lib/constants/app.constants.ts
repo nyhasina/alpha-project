158831964793416cc86b105e511c62dcc3214c9f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 00:38:22 +0300

    Edit default criteria value

--- libs/graph-ql-service/src/lib/constants/app.constants.ts
+++ libs/graph-ql-service/src/lib/constants/app.constants.ts
@@ -1,8 +1,13 @@
-import { Criteria } from '@nicecactus-platform/graph-ql-service';
+import { Criteria, SortOrder } from '@nicecactus-platform/graph-ql-service';
 
 export const DEFAULT_CRITERIA: Criteria<any> = {
     pagination: {
         skip: 0,
         take: 10,
     },
+    sort: {
+        by: 'id',
+        direction: SortOrder.ASC,
+    },
+    search: '',
 };

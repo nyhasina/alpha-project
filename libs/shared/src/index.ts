2fc921373916e866a999b91279db57f72a852827
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 22:05:43 +0300

    Export dialog service and confirmation dialog paylaod interface

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/shared.module';
 export * from './lib/components/paginator/paginator.component';
 export * from './lib/components/search-bar/search-bar.component';
+export * from './lib/services/dialog.service';
+export * from './lib/components/dialogs/confirmation-dialog/confirmation-dialog.component';

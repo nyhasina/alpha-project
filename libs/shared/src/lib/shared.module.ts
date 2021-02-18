cc27e949436567a7b0d4c6aacce00bcab85a6bbc
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 20:32:13 +0300

    Register and export forms module

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { PipesModule } from '@nicecactus-platform/pipes';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
-    imports: [CommonModule, PipesModule, NgSelectModule],
+    imports: [CommonModule, PipesModule, NgSelectModule, FormsModule],
     declarations: [PaginatorComponent, SearchBarComponent],
-    exports: [PaginatorComponent, SearchBarComponent, PipesModule],
+    exports: [PaginatorComponent, SearchBarComponent, PipesModule, FormsModule],
 })
 export class SharedModule {}

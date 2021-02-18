1811334cfe8c3a993982eae4522212cd5e562867
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 20:33:57 +0300

    Implement page size change and searching logic

--- libs/shared/src/lib/components/search-bar/search-bar.component.ts
+++ libs/shared/src/lib/components/search-bar/search-bar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Pagination } from '@nicecactus-platform/graph-ql-service';
+import { Subject, Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 const PAGE_SIZES = [10, 25, 50, 100];
 
@@ -7,6 +10,34 @@ const PAGE_SIZES = [10, 25, 50, 100];
     templateUrl: './search-bar.component.html',
     styleUrls: ['./search-bar.component.scss'],
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit, OnDestroy {
+    @Output() pageChange: EventEmitter<number> = new EventEmitter<number>();
+    @Output() search: EventEmitter<string> = new EventEmitter<string>();
     pageSizes = PAGE_SIZES;
+    pageSize: number;
+    searchTerm: string;
+    searchTermChange$: Subject<string> = new Subject<string>();
+    searchTermChangeSubscription$: Subscription;
+
+    @Input() set initialValue(value: Pagination) {
+        this.pageSize = value.take;
+    }
+
+    ngOnInit() {
+        this.searchTermChangeSubscription$ = this.searchTermChange$.pipe(debounceTime(1000)).subscribe((searchTerm) => {
+            this.search.emit(searchTerm);
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.searchTermChangeSubscription$?.unsubscribe();
+    }
+
+    onPageSizeChange(page: number) {
+        this.pageChange.emit(page);
+    }
+
+    onChange() {
+        this.searchTermChange$.next(this.searchTerm.trim());
+    }
 }

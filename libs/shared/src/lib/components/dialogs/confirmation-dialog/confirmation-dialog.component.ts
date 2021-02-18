8b5090de7b5b0b3f5dad2f957a076230e666c631
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 22:09:09 +0300

    Create confirmation payload interface

--- libs/shared/src/lib/components/dialogs/confirmation-dialog/confirmation-dialog.component.ts
+++ libs/shared/src/lib/components/dialogs/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface ConfirmationPayload {
+    entity: string;
+    id: number;
+    cancel?: string;
+    confirm?: string;
+}
 
 @Component({
-  selector: 'nicecactus-platform-confirmation-dialog',
-  templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.scss']
+    selector: 'nicecactus-platform-confirmation-dialog',
+    templateUrl: './confirmation-dialog.component.html',
+    styleUrls: ['./confirmation-dialog.component.scss'],
 })
-export class ConfirmationDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ConfirmationDialogComponent {
+    constructor(@Inject(MAT_DIALOG_DATA) public data: ConfirmationPayload) {}
 }

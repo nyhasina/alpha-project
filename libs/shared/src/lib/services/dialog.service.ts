7c5da6e75d521d827656053fcf2c848648d81b02
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-18 22:03:16 +0300

    Implement confirmation dialog opening business's logic

--- libs/shared/src/lib/services/dialog.service.ts
+++ libs/shared/src/lib/services/dialog.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import {
+    ConfirmationPayload,
+    ConfirmationDialogComponent,
+} from '../components/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Injectable()
 export class DialogService {
-  constructor(
-    private dialog: MatDialog
-  ) { }
+    private dialogRef: MatDialogRef<any>;
+    constructor(private dialog: MatDialog) {}
+
+    openConfirmationModal(data: ConfirmationPayload) {
+        this.dialogRef = this.dialog.open(ConfirmationDialogComponent, { data });
+        return this.dialogRef.afterClosed();
+    }
 }

b92cc190f3749351ee7b67a910f30afa80ebff19
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-19 21:23:06 +0300

    Register and exprot mat progress spinner module and skeleton list component

--- libs/shared/src/lib/shared.module.ts
+++ libs/shared/src/lib/shared.module.ts
@@ -3,17 +3,28 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { PipesModule } from '@nicecactus-platform/pipes';
 import { PaginatorComponent } from './components/paginator/paginator.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { ConfirmationDialogComponent } from './components/dialogs/confirmation-dialog/confirmation-dialog.component';
 import { DialogService } from './services/dialog.service';
+import { SkeletonListComponent } from './skeleton-list/skeleton-list.component';
 
 @NgModule({
-    imports: [CommonModule, PipesModule, NgSelectModule, FormsModule, MatDialogModule, MatButtonModule],
-    declarations: [PaginatorComponent, SearchBarComponent, ConfirmationDialogComponent],
-    exports: [PaginatorComponent, SearchBarComponent, PipesModule, FormsModule, MatDialogModule, MatButtonModule],
+    imports: [CommonModule, PipesModule, NgSelectModule, FormsModule, MatDialogModule, MatButtonModule, MatProgressSpinnerModule],
+    exports: [
+        PaginatorComponent,
+        SearchBarComponent,
+        PipesModule,
+        FormsModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatProgressSpinnerModule,
+        SkeletonListComponent,
+    ],
+    declarations: [PaginatorComponent, SearchBarComponent, ConfirmationDialogComponent, SkeletonListComponent],
     providers: [DialogService],
 })
 export class SharedModule {}

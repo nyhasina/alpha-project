157f83bd5307a68d9a46435a5bceb0d96a47adee
Manjato Nick <manjato25@gmail.com>
2021-02-21 18:17:59 +0300

    create route  tournament

--- apps/front/src/app/pages/tournament/tournament-routing.module.ts
+++ apps/front/src/app/pages/tournament/tournament-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DetailTournamentComponent } from './detail-tournament/detail-tournament.component';
+import { ListingTournamentComponent } from './listing-tournament/listing-tournament.component';
 import { TournamentComponent } from './tournament.component';
-
 const routes: Routes = [
     {
         path: '',
         component: TournamentComponent,
+    },
+    {
+        path: 'listing-tournament',
+        component: ListingTournamentComponent,
+    },
+    {
+        path: 'detail-tournament',
+        component: DetailTournamentComponent,
     },
 ];
 

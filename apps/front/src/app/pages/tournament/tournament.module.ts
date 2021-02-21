69d3b701cbc6bb557c698baba31ed4f3e53ff6ae
Manjato Nick <manjato25@gmail.com>
2021-02-21 18:14:11 +0300

    create component detail tournament

--- apps/front/src/app/pages/tournament/tournament.module.ts
+++ apps/front/src/app/pages/tournament/tournament.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { TournamentRoutingModule } from './tournament-routing.module';
 import { TournamentComponent } from './tournament.component';
 import { ListingTournamentComponent } from './listing-tournament/listing-tournament.component';
+import { DetailTournamentComponent } from './detail-tournament/detail-tournament.component';
 
 
 @NgModule({
-  declarations: [TournamentComponent, ListingTournamentComponent],
+  declarations: [TournamentComponent, ListingTournamentComponent, DetailTournamentComponent],
   imports: [
     CommonModule,
     TournamentRoutingModule

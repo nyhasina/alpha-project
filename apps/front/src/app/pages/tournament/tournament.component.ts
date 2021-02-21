f3d0ae07aae8bf932254aa05a72ae3000aa32707
Manjato Nick <manjato25@gmail.com>
2021-02-21 20:46:01 +0300

    integration tournament home done

--- apps/front/src/app/pages/tournament/tournament.component.ts
+++ apps/front/src/app/pages/tournament/tournament.component.ts
@@ -3,6 +3,11 @@ interface game {
   src: string,
   plateform: string
 }
+interface tournois {
+  src: string,
+  date: string,
+  name: string
+}
 @Component({
   selector: 'nicecactus-platform-tournament',
   templateUrl: './tournament.component.html',
@@ -26,8 +31,34 @@ export class TournamentComponent implements OnInit {
   {
     src: 'assets/img/liste game/fortnite.jpg',
     plateform: 'PC'
-  },
-]
+  }];
+  tournament: Array<tournois> = [
+{
+  src: 'assets/img/liste game/clash.jpg',
+  date: 'Aujourd\'hui à 21:00:00',
+  name: 'Clash Royal'
+},
+{
+  src: 'assets/img/liste game/fortnite.jpg',
+  date: 'Aujourd\'hui à 21:00:00',
+  name: 'Fortnite'
+},
+{
+  src: 'assets/img/liste game/clash.jpg',
+  date: 'Aujourd\'hui à 21:00:00',
+  name: 'Clash Royal'
+},
+{
+  src: 'assets/img/liste game/fortnite.jpg',
+  date: 'Aujourd\'hui à 21:00:00',
+  name: 'Fortnite'
+},
+{
+  src: 'assets/img/liste game/clash.jpg',
+  date: 'Aujourd\'hui à 21:00:00',
+  name: 'Clash Royal'
+}]
+
   constructor() { }
 
   ngOnInit(): void {

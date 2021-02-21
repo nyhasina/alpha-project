331cfb050d525ef1aaa5654cf5b5417aefd89161
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-21 18:38:52 +0300

    Register profile service

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { ProfileService } from '../profile/profile.service';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-    providers: [PrismaService, UserResolver, UserService],
+    providers: [PrismaService, UserResolver, UserService, ProfileService],
 })
 export class UserModule {}

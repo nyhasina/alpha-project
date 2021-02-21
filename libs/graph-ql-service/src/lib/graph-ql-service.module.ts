5591e73575515eef7452cad01d0ea92432452403
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-21 22:24:19 +0300

    Register currency, language and user service

--- libs/graph-ql-service/src/lib/graph-ql-service.module.ts
+++ libs/graph-ql-service/src/lib/graph-ql-service.module.ts
@@ -6,9 +6,11 @@ import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { createApollo } from './graph-ql.factory';
 import { AuthenticationService } from './services/authentication.service';
+import { CurrencyService } from './services/currency.service';
 import { GameService } from './services/game.service';
+import { LanguageService } from './services/language.service';
 import { PlatformService } from './services/platform.service';
-
+import { UserService } from './services/user.service';
 
 @NgModule({
     imports: [CommonModule, MatSnackBarModule],
@@ -21,6 +23,9 @@ import { PlatformService } from './services/platform.service';
         AuthenticationService,
         PlatformService,
         GameService,
+        CurrencyService,
+        LanguageService,
+        UserService,
     ],
 })
 export class GraphQlServiceModule {}

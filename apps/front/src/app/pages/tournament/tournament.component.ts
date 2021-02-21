db3a3891eb69cfa6eaa3f551c2a56e5d212466b9
Manjato Nick <manjato25@gmail.com>
2021-02-21 19:59:00 +0300

    integration tournament

--- apps/front/src/app/pages/tournament/tournament.component.ts
+++ apps/front/src/app/pages/tournament/tournament.component.ts
@@ -1,12 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+interface game {
+  src: string,
+  plateform: string
+}
 @Component({
   selector: 'nicecactus-platform-tournament',
   templateUrl: './tournament.component.html',
   styleUrls: ['./tournament.component.scss']
 })
-export class TournamentComponent implements OnInit {
 
+export class TournamentComponent implements OnInit {
+  listingGame: Array<game> =
+  [{
+    src: 'assets/img/liste game/a.jpg',
+    plateform: 'Mobile'
+  },
+  {
+    src: 'assets/img/liste game/clash.jpg',
+    plateform: 'PC /Mobile'
+  },
+  {
+    src: 'assets/img/liste game/fortnite.jpg',
+    plateform: 'PC'
+  },
+  {
+    src: 'assets/img/liste game/fortnite.jpg',
+    plateform: 'PC'
+  },
+]
   constructor() { }
 
   ngOnInit(): void {

f15690d56bafb5aed308b867d68f80a48c4798a5
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-22 23:53:05 +0300

    Register team service

--- apps/api/src/app/user/user.module.ts
+++ apps/api/src/app/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { ProfileService } from '../profile/profile.service';
+import { TeamService } from '../team/team.service';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
 
 @Module({
-    providers: [PrismaService, UserResolver, UserService, ProfileService],
+    providers: [PrismaService, UserResolver, UserService, ProfileService, TeamService],
 })
 export class UserModule {}

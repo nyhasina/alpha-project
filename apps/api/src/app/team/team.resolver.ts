c4258930573c7d486be54fb5bc685a11ea765d4f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-22 23:15:48 +0300

    Create team resolver

---
+++ apps/api/src/app/team/team.resolver.ts
@@ -0,0 +1,126 @@
+import { Args, ArgsType, Field, Int, Mutation, Parent, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { GameCountModel, GameModel } from '../game/game.model';
+import { CountArgs, Pagination } from '../shared/models/criteria.model';
+import { UserService } from '../user/user.service';
+import { TEAM_FILTERING } from './team.filters';
+import { TeamModel } from './team.model';
+import { TeamService } from './team.service';
+
+@ArgsType()
+export class CreateTeamInput {
+    @Field()
+    name: string;
+
+    @Field({ nullable: true })
+    tag?: string;
+
+    @Field((type) => [Int], { nullable: true })
+    members?: number[];
+
+    @Field((type) => Int, { nullable: true })
+    owner?: number;
+}
+
+@Resolver((of) => TeamModel)
+export class TeamResolver {
+    constructor(private teamService: TeamService, private userService: UserService) {}
+
+    @ResolveField()
+    async owner(@Parent() team: TeamModel) {
+        const { ownerId } = team;
+        return this.userService.loadUser({ id: ownerId });
+    }
+
+    @ResolveField()
+    async members(@Parent() team: TeamModel) {
+        const { members } = team;
+        return this.userService.loadUsers({
+            where: {
+                id: {
+                    in: members.map((item) => item.id),
+                },
+            },
+        });
+    }
+
+    @Query((returns) => GameCountModel)
+    async teamCount(@Args() countArgs: CountArgs) {
+        const { search } = countArgs;
+        const total = await this.teamService.count({});
+        const count = new GameCountModel(total);
+        return count;
+    }
+
+    @Query((returns) => TeamModel)
+    async team(@Args('id', { type: () => Int }) id: number) {
+        return this.teamService.loadTeam({
+            id,
+        });
+    }
+
+    @Query((returns) => [TeamModel])
+    async teams(@Args() pagination: Pagination) {
+        const { take, skip, by, direction, search } = pagination;
+        let orderBy = {};
+        if (by && direction) {
+            orderBy = { [by]: direction };
+        }
+        return this.teamService.loadTeams({
+            skip,
+            take,
+            where: {
+                OR: [...TEAM_FILTERING(search)],
+            },
+            orderBy,
+        });
+    }
+
+    @Mutation((returns) => TeamModel)
+    async createTeam(@Args() input: CreateTeamInput) {
+        const { name, tag, owner } = input;
+        return this.teamService.createTeam({
+            name,
+            tag,
+            owner: {
+                connect: {
+                    id: owner,
+                },
+            },
+            members: {
+                connect: [
+                    {
+                        id: owner,
+                    },
+                ],
+            },
+        });
+    }
+
+    @Mutation((returns) => TeamModel)
+    async updateTeam(@Args('id', { type: () => Int }) id: number, @Args() input: CreateTeamInput) {
+        const { name, tag, owner, members } = input;
+        return this.teamService.updateTeam({
+            where: {
+                id,
+            },
+            data: {
+                name,
+                tag,
+                owner: {
+                    update: {
+                        id: owner,
+                    },
+                },
+                members: {
+                    set: [],
+                    connect: members.map((item) => ({ id: item })),
+                },
+            },
+        });
+    }
+
+    @Mutation((returns) => TeamModel)
+    async deleteTeam(@Args('id', { type: () => Int }) id: number) {
+        return this.teamService.deleteTeam({ id });
+    }
+}

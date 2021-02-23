77844326787f01ce4a6ef2d55c066ceb98928c81
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-23 11:50:11 +0300

    Remove console log

--- apps/back-office/src/app/layout/administration/components/header/header.component.ts
+++ apps/back-office/src/app/layout/administration/components/header/header.component.ts
@@ -25,7 +25,6 @@ export class HeaderComponent implements OnDestroy {
             )
             .subscribe((_) => {
                 this.breadcrumbs = this.build(this.activatedRoute.root);
-                console.log(this.breadcrumbs);
             });
     }
 

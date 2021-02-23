af4ecc482c89c528ad2577071f672171767b0d4b
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-23 11:08:48 +0300

    Create team queries

---
+++ libs/graph-ql-service/src/lib/queries/team.queries.ts
@@ -0,0 +1,369 @@
+import { gql } from '@apollo/client';
+
+export const LOAD_TEAM_BY_ID = gql`
+    query team($id: Int!) {
+        team(id: $id) {
+            id
+            name
+            tag {
+                id
+                name
+            }
+            owner {
+                id
+                email
+                password
+                profile {
+                    id
+                    firstname
+                    lastname
+                    username
+                    currency {
+                        id
+                        code
+                        label
+                    }
+                    language {
+                        id
+                        code
+                        label
+                    }
+                    currencyId
+                    languageId
+                    deleted
+                }
+                joinedTeams {
+                    id
+                    name
+                    tag {
+                        id
+                        name
+                    }
+                    owner {
+                        id
+                        email
+                        password
+                    }
+                    members {
+                        id
+                        email
+                        password
+                        profile {
+                            id
+                            firstname
+                            lastname
+                            username
+                            currencyId
+                            languageId
+                            deleted
+                        }
+                        joinedTeams {
+                            id
+                            name
+                        }
+                    }
+                }
+            }
+            members {
+                id
+                email
+                password
+            }
+        }
+    }
+`;
+
+export const LOAD_PAGINATED_TEAMS = gql`
+    query teams($take: Int, $skip: Int, $by: String, $direction: String, $search: String) {
+        teams(take: $take, skip: $skip, by: $by, direction: $direction, search: $search) {
+            id
+            name
+            tag {
+                id
+                name
+            }
+            owner {
+                id
+                email
+                password
+                profile {
+                    id
+                    firstname
+                    lastname
+                    username
+                    currency {
+                        id
+                        code
+                        label
+                    }
+                    language {
+                        id
+                        code
+                        label
+                    }
+                    currencyId
+                    languageId
+                    deleted
+                }
+                joinedTeams {
+                    id
+                    name
+                    tag {
+                        id
+                        name
+                    }
+                    owner {
+                        id
+                        email
+                        password
+                    }
+                    members {
+                        id
+                        email
+                        password
+                        profile {
+                            id
+                            firstname
+                            lastname
+                            username
+                            currencyId
+                            languageId
+                            deleted
+                        }
+                        joinedTeams {
+                            id
+                            name
+                        }
+                    }
+                }
+            }
+            members {
+                id
+                email
+                password
+            }
+        }
+        teamCount(search: $search) {
+            total
+        }
+    }
+`;
+
+export const CREATE_TEAM = gql`
+    mutation createTeam($name: String!, $tag: String, $members: [Int!], $owner: Int) {
+        createTeam(name: $name, tag: $tag, members: $members, owner: $owner) {
+            id
+            name
+            tag {
+                id
+                name
+            }
+            owner {
+                id
+                email
+                password
+                profile {
+                    id
+                    firstname
+                    lastname
+                    username
+                    currency {
+                        id
+                        code
+                        label
+                    }
+                    language {
+                        id
+                        code
+                        label
+                    }
+                    currencyId
+                    languageId
+                    deleted
+                }
+                joinedTeams {
+                    id
+                    name
+                    tag {
+                        id
+                        name
+                    }
+                    owner {
+                        id
+                        email
+                        password
+                    }
+                    members {
+                        id
+                        email
+                        password
+                        profile {
+                            id
+                            firstname
+                            lastname
+                            username
+                            currencyId
+                            languageId
+                            deleted
+                        }
+                        joinedTeams {
+                            id
+                            name
+                        }
+                    }
+                }
+            }
+            members {
+                id
+                email
+                password
+            }
+        }
+    }
+`;
+
+export const UPDATE_TEAM = gql`
+    mutation updateTeam($name: String!, $tag: String, $members: [Int!], $owner: Int, $id: Int!) {
+        updateTeam(name: $name, tag: $tag, members: $members, owner: $owner, id: $id) {
+            id
+            name
+            tag {
+                id
+                name
+            }
+            owner {
+                id
+                email
+                password
+                profile {
+                    id
+                    firstname
+                    lastname
+                    username
+                    currency {
+                        id
+                        code
+                        label
+                    }
+                    language {
+                        id
+                        code
+                        label
+                    }
+                    currencyId
+                    languageId
+                    deleted
+                }
+                joinedTeams {
+                    id
+                    name
+                    tag {
+                        id
+                        name
+                    }
+                    owner {
+                        id
+                        email
+                        password
+                    }
+                    members {
+                        id
+                        email
+                        password
+                        profile {
+                            id
+                            firstname
+                            lastname
+                            username
+                            currencyId
+                            languageId
+                            deleted
+                        }
+                        joinedTeams {
+                            id
+                            name
+                        }
+                    }
+                }
+            }
+            members {
+                id
+                email
+                password
+            }
+        }
+    }
+`;
+
+export const DELETE_TEAM = gql`
+    mutation deleteTeam($id: Int!) {
+        deleteTeam(id: $id) {
+            id
+            name
+            tag {
+                id
+                name
+            }
+            owner {
+                id
+                email
+                password
+                profile {
+                    id
+                    firstname
+                    lastname
+                    username
+                    currency {
+                        id
+                        code
+                        label
+                    }
+                    language {
+                        id
+                        code
+                        label
+                    }
+                    currencyId
+                    languageId
+                    deleted
+                }
+                joinedTeams {
+                    id
+                    name
+                    tag {
+                        id
+                        name
+                    }
+                    owner {
+                        id
+                        email
+                        password
+                    }
+                    members {
+                        id
+                        email
+                        password
+                        profile {
+                            id
+                            firstname
+                            lastname
+                            username
+                            currencyId
+                            languageId
+                            deleted
+                        }
+                        joinedTeams {
+                            id
+                            name
+                        }
+                    }
+                }
+            }
+            members {
+                id
+                email
+                password
+            }
+        }
+    }
+`;

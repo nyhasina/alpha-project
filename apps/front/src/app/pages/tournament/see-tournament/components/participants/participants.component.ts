0dc84a2aa813d6ca1bd7737612d78c7aac24a1d3
Manjato Nick <manjato25@gmail.com>
2021-02-23 22:38:31 +0300

    create component

---
+++ apps/front/src/app/pages/tournament/see-tournament/components/participants/participants.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-participants',
+  templateUrl: './participants.component.html',
+  styleUrls: ['./participants.component.scss']
+})
+export class ParticipantsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

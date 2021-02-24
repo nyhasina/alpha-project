8895ef8b9bb09cdd64a5c08549f3b66a2a1787a4
manjato <manjato25@gmail.com>
2021-02-24 15:37:11 +0300

    create edit route detail tournament

--- apps/front/src/app/pages/tournament/tournament-routing.module.ts
+++ apps/front/src/app/pages/tournament/tournament-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
         component: GameListeComponent,
     },
     {
-        path: 'tournament/:idTournament',
+        path: ':idTournament',
         component: SeeTournamentComponent,
     },
 ];

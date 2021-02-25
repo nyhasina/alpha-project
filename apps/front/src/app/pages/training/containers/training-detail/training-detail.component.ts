edb24ee69a12f5d58115866fb701dc7ec9ed50ac
manjato <manjato25@gmail.com>
2021-02-25 17:26:33 +0300

    create component training detail

---
+++ apps/front/src/app/pages/training/containers/training-detail/training-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-training-detail',
+  templateUrl: './training-detail.component.html',
+  styleUrls: ['./training-detail.component.scss']
+})
+export class TrainingDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- apps/front/src/app/pages/training/training.module.ts
+++ apps/front/src/app/pages/training/training.module.ts
@@ -7,8 +7,9 @@ import { ProgramRootComponent } from './containers/program-root/program-root.com
 import { SkillGamesRootComponent } from './containers/skill-games-root/skill-games-root.component';
 import { InputProgramComponent } from './containers/input-program/input-program.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { TrainingDetailComponent } from './containers/training-detail/training-detail.component';
 @NgModule({
-    declarations: [ProgramRootComponent, StatisticRootComponent, SkillGamesRootComponent, InputProgramComponent],
+    declarations: [ProgramRootComponent, StatisticRootComponent, SkillGamesRootComponent, InputProgramComponent, TrainingDetailComponent],
     imports: [CommonModule, TrainingRoutingModule, MatProgressBarModule],
 })
 export class TrainingModule {}

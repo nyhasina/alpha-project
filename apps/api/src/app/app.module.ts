25801df48db69af72397140790646094afca2d62
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-02-28 23:54:16 +0300

    register statement module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { LanguageModule } from './language/language.module';
 import { PlatformModule } from './platform/platform.module';
 import { ProfileModule } from './profile/profile.module';
 import { GqlAuthGuard } from './shared/decorators/gql-auth-guard.decorator';
+import { StatementModule } from './statement/statement.module';
 import { TagModule } from './tag/tag.module';
 import { TeamModule } from './team/team.module';
 import { TournamentRewardModule } from './tournament-reward/tournament-reward.module';
@@ -39,6 +40,7 @@ import { UserModule } from './user/user.module';
         InvitationModule,
         TournamentRewardModule,
         TournamentTypeModule,
+        StatementModule,
     ],
     providers: [
         {

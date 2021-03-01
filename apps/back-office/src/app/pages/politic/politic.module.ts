bb0fbbc9e25c5c1ae6c1862e9732a34fa927fd11
Manjato Nick <manjato25@gmail.com>
2021-03-01 22:36:38 +0300

    create route politique

--- apps/back-office/src/app/pages/politic/politic.module.ts
+++ apps/back-office/src/app/pages/politic/politic.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { PoliticRoutingModule } from './politic-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    PoliticRoutingModule
   ]
 })
 export class PoliticModule { }

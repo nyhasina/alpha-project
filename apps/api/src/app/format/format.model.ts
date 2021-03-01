eae5ee6f6a78d9e434f7326c1879e915775ce435
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-01 21:22:20 +0300

    Create format model

---
+++ apps/api/src/app/format/format.model.ts
@@ -0,0 +1,10 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+export class FormatModel {
+    @Field((returns) => Int)
+    id: number;
+
+    @Field()
+    name: string;
+}

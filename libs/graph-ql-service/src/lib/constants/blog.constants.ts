dbfeba2c46d64c11cb4eb27e70af4c12e193e39d
Manjato Nick <manjato25@gmail.com>
2021-03-07 20:22:37 +0300

    blog constant

---
+++ libs/graph-ql-service/src/lib/constants/blog.constants.ts
@@ -0,0 +1,10 @@
+import { Blog } from '@nicecactus-platform/graph-ql-service';
+
+export const EMPTY_BLOG: Blog = {
+    id: null,
+    content: null,
+    name: null,
+    isRemoved: false,
+    video: null,
+    cover: null
+};

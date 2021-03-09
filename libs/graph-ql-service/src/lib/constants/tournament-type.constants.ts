038ca7eb79f5da3dd7ca3619c0f4c50165a664e6
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-09 21:17:17 +0300

    Create tournament type constant

---
+++ libs/graph-ql-service/src/lib/constants/tournament-type.constants.ts
@@ -0,0 +1,7 @@
+import { TournamentType } from '../interfaces/tournament-type.interface';
+
+export const EMPTY_TOURNAMENT_TYPE: TournamentType = {
+    id: null,
+    name: null,
+    reward: null,
+};

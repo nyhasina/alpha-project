a999cc1fd1ff7f358aacdeedf0af05897d5bc715
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-10 23:56:23 +0300

    Export ellipsis pipe

--- libs/pipes/src/lib/pipes.module.ts
+++ libs/pipes/src/lib/pipes.module.ts
@@ -1,13 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EllipsisPipe } from './ellipsis.pipe';
 import { FirstLetterPipe } from './first-letter.pipe';
 import { ObjectToStringPipe } from './objet-to-string.pipe';
-import { EllipsisPipe } from './ellipsis.pipe';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [FirstLetterPipe, ObjectToStringPipe, EllipsisPipe],
-  exports: [FirstLetterPipe, ObjectToStringPipe]
+  exports: [FirstLetterPipe, ObjectToStringPipe, EllipsisPipe]
 })
 export class PipesModule {
 }

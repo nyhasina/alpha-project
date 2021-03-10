13aaada9e83121a8eabaad7fe9574d0f02b1ad4a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-10 23:53:36 +0300

    Create ellipsis pipe

--- libs/pipes/src/lib/pipes.module.ts
+++ libs/pipes/src/lib/pipes.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FirstLetterPipe } from './first-letter.pipe';
 import { ObjectToStringPipe } from './objet-to-string.pipe';
+import { EllipsisPipe } from './ellipsis.pipe';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [FirstLetterPipe, ObjectToStringPipe],
+  declarations: [FirstLetterPipe, ObjectToStringPipe, EllipsisPipe],
   exports: [FirstLetterPipe, ObjectToStringPipe]
 })
 export class PipesModule {

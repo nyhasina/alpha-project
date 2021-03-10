2b2d6ff1fab6c4b472ba2edd7375d38901e0f79c
manjato <manjato25@gmail.com>
2021-03-10 17:39:00 +0300

    create params for rules

--- apps/front/src/app/pages/tournament/see-tournament/components/rules/rules.component.ts
+++ apps/front/src/app/pages/tournament/see-tournament/components/rules/rules.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 
 import {MatAccordion} from '@angular/material/expansion';
+interface iRules {
+  titre: string,
+  content: string
+}
 @Component({
   selector: 'nicecactus-platform-rules',
   templateUrl: './rules.component.html',
   styleUrls: ['./rules.component.scss']
 })
+
 export class RulesComponent implements OnInit {
+  bench: boolean = false;
+  rules: Array<iRules> = [
+
+  ]
   constructor() { }
 
   ngOnInit(): void {

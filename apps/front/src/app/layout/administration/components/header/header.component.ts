182be2197f800f57b896608059729b871ee01822
Manjato Nick <manjato25@gmail.com>
2021-03-13 21:09:59 +0300

    header dynamic

--- apps/front/src/app/layout/administration/components/header/header.component.ts
+++ apps/front/src/app/layout/administration/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'nicecactus-platform-header',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  selected: string;
+  constructor(private route: Router) {
+    this.route.events.pipe(
+      filter(event => event instanceof NavigationEnd)  
+    ).subscribe((event: NavigationEnd) => {
+      console.log(event.url);
+      this.selected = event.url.split('/').pop()
+      if(this.selected === 'r%C3%A8gles'){
+        this.selected = 'règles'
+      }
+    });
+   }
   ngOnInit(): void {
+   
   }
 
 }

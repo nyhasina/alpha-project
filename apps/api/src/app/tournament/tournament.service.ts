2a9e24ca4d18719670c4987abe3c77b27d2c3946
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-14 00:02:09 +0300

    Implement number of rounds and participants per match computation

--- apps/api/src/app/tournament/tournament.service.ts
+++ apps/api/src/app/tournament/tournament.service.ts
@@ -100,6 +100,29 @@ export class TournamentService {
         });
     }
 
+    private async computeRounds(teams: number, teamPerMatch: number): Promise<number> {
+        if (teams <= 1) {
+            return 0;
+        }
+        teams = Math.trunc(teams / teamPerMatch);
+        return teams + await this.computeRounds(teams, teamPerMatch);
+    }
+
+    private computeNearestPowerOf2(x: number) {
+        if (x <= 0) {
+            // If x <= 0, directly return 2 to the 0 power
+            return 1;
+        } else if ((x & (x - 1)) == 0) {
+            // Judging by the "bitwise AND" operation, if x is a power of 2, directly return x
+            return x;
+        } else {
+            // Calculate the smallest integer greater than the logarithm of x with base 2
+            // For example, x = 25, the logarithm of 25 with 2 as the base is 4.643..., the result of forcibly converting to int and adding 1 is 5
+            const n = Math.trunc(Math.log(x) / Math.log(2)) + 1;
+            return Math.pow(2, n);
+        }
+    }
+
     async closeRegistration(tournamentId: number): Promise<Tournament> {
         const tournament: Partial<TournamentModel> = await this.loadTournament({ id: tournamentId });
         if (!tournament) {
@@ -111,16 +134,10 @@ export class TournamentService {
                 HttpStatus.NOT_FOUND
             );
         }
-        console.log(tournament.teams);
-        if (tournament.teams.length % 2 !== 0) {
-            throw new HttpException(
-                {
-                    status: HttpStatus.UNPROCESSABLE_ENTITY,
-                    error: 'Participants number must be even',
-                },
-                HttpStatus.UNPROCESSABLE_ENTITY
-            );
-        }
+        console.log(`${tournament.teams.map((item) => item.id).join(',')}: ${tournament.teams.length}`);
+        const nextPowerOf2 = this.computeNearestPowerOf2(tournament.teams.length);
+        console.log(nextPowerOf2 - tournament.teams.length);
+        console.log(await this.computeRounds(tournament.teams.length, 2));
         return this.updateTournament({
             data: {
                 closed: true,

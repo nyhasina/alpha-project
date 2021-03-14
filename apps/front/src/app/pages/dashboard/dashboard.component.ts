8ae10e852f4c8560dbd385606e6375e38e86d3cb
Manjato Nick <manjato25@gmail.com>
2021-03-14 17:42:34 +0300

    add tab dashboard

--- apps/front/src/app/pages/dashboard/dashboard.component.ts
+++ apps/front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormControl} from '@angular/forms';
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+    tabs = ['First', 'Second', 'Third'];
+    selected = new FormControl(0);
     constructor() {}
-
+    addTab(selectAfterAdding: boolean) {
+        this.tabs.push('New');
+    
+        if (selectAfterAdding) {
+          this.selected.setValue(this.tabs.length - 1);
+        }
+      }
+    
+      removeTab(index: number) {
+        this.tabs.splice(index, 1);
+      }
     ngOnInit(): void {}
 }
--- apps/front/src/app/pages/dashboard/dashboard.module.ts
+++ apps/front/src/app/pages/dashboard/dashboard.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-
+import {MatTabsModule} from '@angular/material/tabs';
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, DashboardRoutingModule],
+    imports: [CommonModule, DashboardRoutingModule, MatTabsModule],
 })
 export class DashboardModule {}

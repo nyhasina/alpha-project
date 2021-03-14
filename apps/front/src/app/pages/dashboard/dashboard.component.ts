da1b65c6fd3d153919f38d8c47e8d6f033c077a1
Manjato Nick <manjato25@gmail.com>
2021-03-14 18:33:14 +0300

    set games list

--- apps/front/src/app/pages/dashboard/dashboard.component.ts
+++ apps/front/src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl} from '@angular/forms';
+
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
@@ -7,11 +8,14 @@ import {FormControl} from '@angular/forms';
 })
 export class DashboardComponent implements OnInit {
     tabs = ['First', 'Second', 'Third'];
-    selected = new FormControl(0);
+    selected = new FormControl(1);
+    // eslint-disable-next-line max-len
+    games: Array<string> = ['Apex Legends', 'Auto Chess', 'Brawl Stars', 'Brawlhalla', 'Call of Duty', 'Call of duty Mobile', 'Call of duty Modern of War', 'Clash Royal', 'Fifa 21', 'Fortnite'];
+
     constructor() {}
     addTab(selectAfterAdding: boolean) {
         this.tabs.push('New');
-    
+      console.log(this.selected.setValue)
         if (selectAfterAdding) {
           this.selected.setValue(this.tabs.length - 1);
         }

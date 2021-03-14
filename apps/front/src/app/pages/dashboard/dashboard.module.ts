b99ab1b52080efa7551c0a417693986b5979dd32
Manjato Nick <manjato25@gmail.com>
2021-03-14 18:24:41 +0300

    add module in dashboard

--- apps/front/src/app/pages/dashboard/dashboard.module.ts
+++ apps/front/src/app/pages/dashboard/dashboard.module.ts
@@ -4,8 +4,12 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+import {MatInputModule} from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, DashboardRoutingModule, MatTabsModule],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule, DashboardRoutingModule, MatTabsModule, MatCheckboxModule, MatInputModule],
 })
 export class DashboardModule {}

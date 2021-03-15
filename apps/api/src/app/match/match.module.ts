daf6e7056f712d16832cc1e67fb54457758b60dd
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-15 20:09:03 +0300

    Provide team and round service

--- apps/api/src/app/match/match.module.ts
+++ apps/api/src/app/match/match.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { RoundService } from '../round/round.service';
+import { TeamService } from '../team/team.service';
 import { MatchService } from './match.service';
 
 @Module({
-    providers: [PrismaService, MatchService],
+    providers: [PrismaService, MatchService, TeamService, RoundService],
 })
 export class MatchModule {}

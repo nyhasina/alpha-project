8b61581f5278afa4847dc65802209f3ca2fd3f9a
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-15 21:18:04 +0300

    Provide match resolver

--- apps/api/src/app/match/match.module.ts
+++ apps/api/src/app/match/match.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { RoundService } from '../round/round.service';
 import { TeamService } from '../team/team.service';
+import { MatchResolver } from './match.resolver';
 import { MatchService } from './match.service';
 
 @Module({
-    providers: [PrismaService, MatchService, TeamService, RoundService],
+    providers: [PrismaService, MatchResolver, MatchService, TeamService, RoundService],
 })
 export class MatchModule {}

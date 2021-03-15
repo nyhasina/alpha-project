005a2773abe4141c75e18925057333388ea35b33
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-15 20:10:19 +0300

    Replace rank by round

--- apps/api/src/app/match/match.model.ts
+++ apps/api/src/app/match/match.model.ts
@@ -1,6 +1,22 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { TeamModel } from '../team/team.model';
-import { TournamentModel } from '../tournament/tournament.model';
+
+export class CreateMatchInput {
+    @Field((returns) => Int)
+    teamA: number;
+
+    @Field((returns) => Int)
+    teamB: number;
+
+    @Field((returns) => Int)
+    round: number;
+
+    constructor(teamA: number, teamB: number, round: number) {
+        this.teamA = teamA;
+        this.teamB = teamB;
+        this.round = round;
+    }
+}
 
 @ObjectType()
 export class MatchModel {
@@ -8,7 +24,9 @@ export class MatchModel {
     id: number;
 
     @Field((returns) => Int)
-    rank: number;
+    round: number;
+
+    roundId?: number;
 
     @Field((returns) => TeamModel)
     teamA: TeamModel;

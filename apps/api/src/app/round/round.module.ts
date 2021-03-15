ec24931032dcf1c2fd496514006a17c7009b324f
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2021-03-15 21:18:42 +0300

    Provide round resolver

--- apps/api/src/app/round/round.module.ts
+++ apps/api/src/app/round/round.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MatchService } from '../match/match.service';
 import { PrismaService } from '../prisma/prisma.service';
 import { TournamentService } from '../tournament/tournament.service';
+import { RoundResolver } from './round.resolver';
 import { RoundService } from './round.service';
 
 @Module({
-    providers: [PrismaService, RoundService, TournamentService, MatchService],
+    providers: [PrismaService, RoundResolver, RoundService, TournamentService, MatchService],
 })
 export class RoundModule {}

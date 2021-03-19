127e16dbb1bb722bf7d634220629420760911279
manjato <manjato25@gmail.com>
2021-03-19 18:32:30 +0300

    create component team

---
+++ apps/front/src/app/pages/team/component/detail-team/detail-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nicecactus-platform-detail-team',
+  templateUrl: './detail-team.component.html',
+  styleUrls: ['./detail-team.component.scss']
+})
+export class DetailTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

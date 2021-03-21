c760a7956ede9eb8314959852440522ff9d19bc5
Manjato Nick <manjato25@gmail.com>
2021-03-21 23:16:25 +0300

    dynamic team

--- apps/front/src/app/pages/team/component/liste-team/liste-team.component.ts
+++ apps/front/src/app/pages/team/component/liste-team/liste-team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'nicecactus-platform-liste-team',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-team.component.scss']
 })
 export class ListeTeamComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  modalAddTeam = null;
+  name = new FormControl('', [Validators.required]);
+  tag = new FormControl('', [Validators.required]);
+  formTeam: FormGroup;
+  tabsTeam: Array<any> = []
+  constructor(
+    config: NgbModalConfig,
+    private modalService: NgbModal,
+    private formBuilder?: FormBuilder) {
+    config.backdrop = 'static';
+    config.keyboard = false;
+    this.createTeam()
   }
-
+  createTeam() {
+    this.formTeam = this.formBuilder.group({
+      name: ['', Validators.required],
+      tag: ['', Validators.required],
+  });
+  }
+  get f() { return this.formTeam.controls; }
+  open(content) {
+    this.modalAddTeam = this.modalService.open(content, {centered: true, size: 'sm' });
+   }
+   confirm() {
+     const tab = {
+       name: this.formTeam.value.name,
+       tag: this.formTeam.value.tag,
+       date: Date.now(),
+       owner: 'John Doe'
+     }
+     this.tabsTeam.push(tab)
+     this.formTeam.reset()
+     this.modalAddTeam.close()
+   }
 }
